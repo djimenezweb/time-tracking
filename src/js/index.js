b8faf9aa7e5eb2fba29e453aca72ea2c1f1efd5d
Daniel Jiménez <djimenezweb@gmail.com>
2023-02-01 18:59:27 +0100

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,26 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-sayHello();
+const cardListElement = document.getElementById('card-list');
+
+const fetchData = url => fetch(url);
+
+const printData = (filter, filter2) => {
+  const request = fetchData('../data/data.json');
+  request
+    .then(response => response.json())
+    .then(data => {
+      const allCurrent = document.querySelectorAll('.card__current');
+      const allPrevious = document.querySelectorAll('.card__previous');
+      allCurrent.forEach((element, index) => {
+        element.textContent = data[index].timeframes[filter].current + 'hrs';
+        allPrevious[index].textContent = `${filter2} - ${data[index].timeframes[filter].previous}hrs`;
+      });
+    });
+};
+
+printData('weekly');
+
+cardListElement.addEventListener('click', ev => {
+  printData(ev.target.dataset.filter, ev.target.dataset.filter2);
+});
