1f9ba5201771f0f24a213a8eebdcb6fc4fab2064
Daniel Jiménez <djimenezweb@gmail.com>
2023-02-15 17:31:32 +0100

    fixed vite template

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,22 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+import data from '../data/data.json';
 
 const cardListElement = document.getElementById('card-list');
 
 const fetchData = url => fetch(url);
 
 const printData = (filter, filter2) => {
-  const request = fetchData('../data/data.json');
+  /* const request = fetchData('../data/data.json');
   request
     .then(response => response.json())
-    .then(data => {
-      const allCurrent = document.querySelectorAll('.card__current');
-      const allPrevious = document.querySelectorAll('.card__previous');
-      allCurrent.forEach((element, index) => {
-        element.textContent = data[index].timeframes[filter].current + 'hrs';
-        allPrevious[index].textContent = `${filter2} - ${data[index].timeframes[filter].previous}hrs`;
-      });
-    });
+    .then(data => { */
+  const allCurrent = document.querySelectorAll('.card__current');
+  const allPrevious = document.querySelectorAll('.card__previous');
+  allCurrent.forEach((element, index) => {
+    element.textContent = data[index].timeframes[filter].current + 'hrs';
+    allPrevious[index].textContent = `${filter2} - ${data[index].timeframes[filter].previous}hrs`;
+  });
+  //});
 };
 
 printData('weekly', 'Last week');
